a6d91d2ddaccc6f672f0801c49214e25fc5e8b06
Linda Miles <lindagomiles@gmail.com>
2022-09-24 20:33:52 +0200

    creating loops

---
+++ number.js
@@ -0,0 +1,29 @@
+const counter = document.querySelector("#counter");
+const btns = document.querySelectorAll(".btn");
+let count = 0;
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const style = e.currentTarget.classList;
+
+    if (style.contains("increase")) {
+      count++;
+    } else if (style.contains("decrease")) {
+      count--;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      counter.style.color = "green";
+    }
+
+    if (count < 0) {
+      counter.style.color = "red";
+    }
+
+    if (count == 0) {
+      counter.style.color = "black";
+    }
+    counter.textContent = count;
+  });
+});
